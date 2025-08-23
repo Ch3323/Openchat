f44da0f88f898b73224e62fdca240c8dcaf4078f
Ittipat <ittipat145@gmail.com>
2025-08-23 21:12:50 +0700

    T.T

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,12 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
-import prettierPlugin from 'eslint-plugin-prettier';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+import { FlatCompat } from '@eslint/eslintrc'
+ 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-export default [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-      prettier: prettierPlugin,
-    },
+  baseDirectory: import.meta.dirname,
+})
+ 
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next'],
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-unused-expressions': 'off',
@@ -38,5 +26,7 @@ export default [
 
       'prettier/prettier': ['warn', { endOfLine: 'auto' }],
     },
-  },
-];
+  }),
+]
+ 
+export default eslintConfig
